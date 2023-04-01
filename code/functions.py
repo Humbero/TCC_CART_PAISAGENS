8fa05e2bd5cb87f1bc0ae1631b2829e4ebdee4d8
Humberto Correa Araújo <humbertocorrea93@gmail.com>
2023-03-31 21:01:55 -0300

    :construction: fix :bug:
    Carga de csv e tratamento de dados em pleno funcionamento
    OBS: escrita em arquivo shape apresenta problemas, pois não é possível escrever o data frame com codificação utf-8 usando o geopandas. Possíveis soluções: Buscar outra biblioteca ou editar csv de entrada.

--- code/functions.py
+++ code/functions.py
@@ -5,7 +5,7 @@ Project: Otimização de etapas em cartografia de paisagens
 
 #imports
 import pandas as pd
-import pandera as pa
+#import pandera as pa
 import re
 import geopandas as gpd
 from shapely.geometry import Point
@@ -41,7 +41,7 @@ def open_csv():
 
     #carga do csv em um DataFrame 
     #OBS: foi utilizado o encoding ISO-8859-1 pois o utf-8 não estava conseguindo decodificar os caracteres especiais da língua portuguesa.
-    df_main = pd.read_csv(caminho_final, encoding='ISO-8859-1', sep=';')
+    df_main = pd.read_csv(caminho_final, encoding='utf-16', sep=',')
 
     #retornando ao main o dataframe carregado
     return df_main
@@ -53,56 +53,48 @@ def creat_shape(df_entrada):
 
     #Criação da geometria a ser utilizada na produção do arquivo shape, neste caso a geometria é do tipo Point, onde são passados como x,y respectivamente as coordenadas de longitude e latitude
     #extraidas do csv (OBS: o nome das colunas deve ser igual ao nome apresentado na coluna do dataframe de entrada, por isso utilizar o arquivo csv definido como padrão de entrada no programa)
-    geometry = [Point(xy) for xy in zip(df_entrada['LONGITUDE'], df_entrada['LATITUDE'])]
+    geometry = [Point(xy) for xy in zip(df_entrada['longitude'], df_entrada['latitude'])]
 
     #criação de dataframe contendo dados geográficos passando os parâmetros de geometria e dataframe de entrada da função
     gdf_point = gpd.GeoDataFrame(df_entrada, geometry= geometry)
 
+    #obetenção do nome do arquivo
+    caminho_shp = input('Digite o caminho onde deseja salvar o arquivo, ex: C:\pasta_x\pasta_b: ')
+    nome_shp = input('Digite o nome que deseja dar ao arquivo do tipo shapefile: ')
+
     #chamada do método .to_file para criação do arquivo com base no geodataframe criado anteriormente
-    gdf_point.to_file(r'C:\TCC_ETL_CART_PAISAGENS\teste_abertura_csv.shp', driver='ESRI Shapefile')
+    gdf_point.to_file(r'{}'.format(caminho_shp + '/'+ nome_shp), driver='ESRI Shapefile', encoding= 'utf-16-le', bom=True)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Função para validar os dados do data frame
 def validar_dados_df(df_validacao):
 
+    df_valido = df_validacao
 
-    #Alteração dos dados de latitude e longitude para adoção do ponto como marcador de unidade no dataframe fornecido a função
-    df_validacao['Longitude'] = df_validacao['Longitude'].str.replace(',','.')
+    #etapa 01: colocar todos os títulos de colunas em letras minúsculas para evitar problemas de processamento futuro
+    df_valido.columns = [col.lower() for col in df_valido.columns]
 
+    #etapa 02: converter todos as ',' em '.' nas colunas latitude e longitude
+    df_valido['longitude'] = df_valido['longitude'].str.replace(',','.')
+    df_valido['latitude'] = df_valido['latitude'].str.replace(',','.')
 
-    schema = pa.DataFrameSchema(
+    #etapa 03: garantir que as colunas latitude e longitude esejam do tipo float e que a coluna parcela seja do tipo inteiro
+    df_valido['longitude'] = df_valido['longitude'].astype(float)
+    df_valido['latitude'] = df_valido['latitude'].astype(float)
+    df_valido['parcela'] = df_valido['parcela'].astype(int)
 
-        columns= {
-        
-            'Parcela': pa.Column(pa.Int),
-            'Latitude': pa.Column(pa.Float),
-            'Longitude': pa.Column(pa.Float),
-            'Fisionomia': pa.Column(pa.String),
-            'Complementos': pa.Column(pa.String),
-            'Ambiente': pa.Column(pa.String),
-            'Lenhosa dominante': pa.Column(pa.String),
-            'Herbacea dominante': pa.Column(pa.String),
-            'Estiagem': pa.Column(pa.String, nullable=True),
-            'Erosao': pa.Column(pa.String, nullable=True),
-            'Fogo': pa.Column(pa.String, nullable=True),
-            'Desmatamento': pa.Column(pa.String, nullable=True),
-            'Pastoreio/herbivoria': pa.Column(pa.String, nullable=True),
-            'Inundacao': pa.Column(pa.String, nullable=True),
-            'Observacao': pa.Column(pa.String, nullable=True)
-        
-            }
+    #etapa 04: colocar todos os dados do tipo string do data frame em letras minúsculas
+    df_valido = df_valido.applymap(lambda s: s.lower() if type(s) == str else s)
 
+    return df_valido
 
-    )
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 
 #trecho de código exclusivamente utilizado para testas as funções implementadas
-df_teste = pd.read_csv( r'C:\TCC_ETL_CART_PAISAGENS\teste_abertura_csv.csv',sep=',',encoding='ISO-8859-1')
-
-df_teste['LONGITUDE'] = df_teste['LONGITUDE'].str.replace(',', '.')
-df_teste['LATITUDE'] = df_teste['LATITUDE'].str.replace(',', '.')
 
 
 
--- code/main.py
+++ code/main.py
@@ -5,7 +5,19 @@ Project: Otimização de etapas em cartografia de paisagens
 
 #imports
 import functions as fun
+import pandas as pd
+
+
+df_abertura = pd.read_csv(r'C:\TCC_ETL_CART_PAISAGENS\CSV_TCC_V0013.csv', sep=',',encoding='utf-16')
+
+#df_abertura = fun.open_csv()
+
+print(df_abertura.head(4))
 
 
 
+df_validado = fun.validar_dados_df(df_abertura)
 
+print(df_validado.head(5))
+
+fun.creat_shape(df_validado)
