c1d84c7674537bdc3f382a4906e02d19ce26fb55
Humberto Correa Araújo <humbertocorrea93@gmail.com>
2023-04-10 21:34:05 -0300

    :construction: Construção
    Corrigido erro em criação de shape (substituição por GPKG)
    Criação de interface com o usuário
    Correção de eficiência em algumas funções

--- code/functions.py
+++ code/functions.py
@@ -5,51 +5,30 @@ Project: Otimização de etapas em cartografia de paisagens
 
 #imports
 import pandas as pd
-#import pandera as pa
 import re
 import geopandas as gpd
 from shapely.geometry import Point
+import PySimpleGUI as sg
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #função para abertura do arquivo csv e carga do DataFrame no main
-def open_csv():
-
-    #variáveis de uso
-    df_main = 0
-    validador = True
-    caminho_final = 0
-
-    #definição e teste do caminho e nome do arquivo a ser carregado
-    while  validador == True:
-
-        print('Para a carga do arquivo .CSV precisaremos de algumas informações, favor preecher com atenção!')
-        caminho_temp = input('Digite o caminho da pasta onde o arquivo foi armazenado, exempo de caminho: C:\pasta_de_joão\desktop\pasta_projeto \nDigite o caminho da pasta:  ')
-        nome_arquivo = input('Forneca o nome do arquivo com sua extensão, exemplo: arquivo_x.csv \nDigite o nome do arquivo: ')
-
-        #confirmação dos dados
-        print('Favor confirme se o caminh do arquivo é: '+caminho_temp+' e o nome do arquivo é:  '+nome_arquivo)
-
-        confirma = input('Digite C para confirmar ou  A para alterar os dados: ')
-
-        if confirma in 'Cc':
-
-            #utilizada a função r'{}'.format para tornar o o caminho final do tipo string real, sem que a barra invertida pudesse comprometer o resutlado de construção do código
-            caminho_final =r'{}'.format(caminho_temp  +'/'+ nome_arquivo) 
-            
-            validador = False
+#Entradas da função: nenhuma
+#Retorno da função: dataframe codificado em utf-8
+def open_csv(caminho_e_nome_r):
 
     #carga do csv em um DataFrame 
-    #OBS: foi utilizado o encoding ISO-8859-1 pois o utf-8 não estava conseguindo decodificar os caracteres especiais da língua portuguesa.
-    df_main = pd.read_csv(caminho_final, encoding='lantin1', sep=',')
+    df_main = pd.read_csv(caminho_e_nome_r, encoding='utf-8', sep=';')
 
     #retornando ao main o dataframe carregado
     return df_main
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Função para criação de arquivo do tipo shapefile baseando-se em um dataframe existente que será passado como parâmetro de entrada
-def creat_shape(df_entrada):
+#Função para criação de arquivo do tipo geopackge baseando-se em um dataframe existente que será passado como parâmetro de entrada
+#entradas da função: dataframe contendo colunas 'latitude' e 'longitude'
+#Retrono da função: mensagem de confirmação
+def creat_geopackge(df_entrada,caminho_e_nome_w):
 
     #Criação da geometria a ser utilizada na produção do arquivo shape, neste caso a geometria é do tipo Point, onde são passados como x,y respectivamente as coordenadas de longitude e latitude
     #extraidas do csv (OBS: o nome das colunas deve ser igual ao nome apresentado na coluna do dataframe de entrada, por isso utilizar o arquivo csv definido como padrão de entrada no programa)
@@ -58,16 +37,16 @@ def creat_shape(df_entrada):
     #criação de dataframe contendo dados geográficos passando os parâmetros de geometria e dataframe de entrada da função
     gdf_point = gpd.GeoDataFrame(df_entrada, geometry= geometry)
 
-    #obetenção do nome do arquivo
-    caminho_shp = input('Digite o caminho onde deseja salvar o arquivo, ex: C:\pasta_x\pasta_b: ')
-    nome_shp = input('Digite o nome que deseja dar ao arquivo do tipo shapefile: ')
-
     #chamada do método .to_file para criação do arquivo com base no geodataframe criado anteriormente
-    gdf_point.to_file(r'{}'.format(caminho_shp + '/'+ nome_shp), driver='ESRI Shapefile', encoding= 'utf-16-le', bom=True)
+    gdf_point.to_file(r'{}'.format(caminho_e_nome_w+'.gpkg'), driver='GPKG', encoding= 'utf-8')
+
+    
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Função para validar os dados do data frame
+#Entrada fa função: dataframe
+#Retorno da função: dataframe padrinizado
 def validar_dados_df(df_validacao):
 
     df_valido = df_validacao
@@ -92,6 +71,8 @@ def validar_dados_df(df_validacao):
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Função para composição de sugestões de nomenclatura as parcelas coletadas em campo seguindo o método constante no livro de cartografia de paisagens (Cavalcanti,L.C.S, 2018)
+#Entrada da função: Dataframe
+#retorno da função: dataframe com adição da coluna paisagem com as sugestões de nomenclatura da paisagem
 def paisagem_nome(df_entrada):
 
     df_nomear = df_entrada
@@ -102,53 +83,181 @@ def paisagem_nome(df_entrada):
     #etapa 02 preenchimento do data frame como a composição do nome da paisagem
     for i, row in df_nomear.iterrows():
 
-        #etapa 03 estrutura de decisão para definição das duas perturbações de maior impacto na paisagem
         
-        #estruturação das variaveis do dataframe para montar estrutura de seleção e conversão
-        estiagem = df_nomear['estiagem'][i]
-        erosao = df_nomear['erosão'][i]
-        fogo = df_nomear['fogo'][i]
-        desmatamento = df_nomear['desmatamento'][i]
-        pastoreio = df_nomear['pastoreio/herbivoria'][i]
-        inundacao = df_nomear['inundação'][i]
-        perturbacao_final = []
+        #Validando que a paisagem possua as informações mínimas a composição da paisagem em cada coluna de acordo com o padrão da ficha de campo e seus respectivos itens de menor tamaho
+        if not df_nomear['fisionomia'][i] or len(df_nomear['fisionomia'][i]) < 7 or not df_nomear['complementos'][i] or len(df_nomear['complementos'][i]) < 5 or not df_nomear['ambiente'][i] or len(df_nomear['ambiente'][i]) < 4:
 
 
-        ##teste de classificação para as duas perturbações mais influentes, começando pelo caso 0, onde nenhuma perturbação foi identificada
-        if estiagem == 0 and erosao == 0 and fogo == 0 and desmatamento == 0 and pastoreio == 0 and inundacao == 0:
+            df_nomear.loc[i,'paisagem'] = 'dados insuficentes para nomenclatura'
 
-            perturbacao_final = 'sem perturbações visíveis'
-        
         else:
-            #para facilitar a seleção entre as 6 variáveis foi criado um dicionário que posteriomente será classificado e extraído os dois maiores valores
-            seletor = {'estiagem': estiagem, 'erosão': erosao,'fogo': fogo, 'desmatamento': desmatamento, 'pastoreio/herbivoria': pastoreio, 'inundação': inundacao}
 
-            seletor_ordenado = sorted(seletor.items(), key=lambda x: x[1], reverse=True)
+            #etapa 03 estrutura de decisão para definição das duas perturbações de maior impacto na paisagem
+            
+            #estruturação das variaveis do dataframe para montar estrutura de seleção e conversão
+            estiagem = df_nomear['estiagem'][i]
+            erosao = df_nomear['erosão'][i]
+            fogo = df_nomear['fogo'][i]
+            desmatamento = df_nomear['desmatamento'][i]
+            pastoreio = df_nomear['pastoreio/herbivoria'][i]
+            inundacao = df_nomear['inundação'][i]
+            perturbacao_final = []
 
-            selec_final = seletor_ordenado[:2]
-            selec_final_dicionario = dict(selec_final)
-        
-            #for para percorrer o dicionário contendo as perturbações de maior influência e substituir os valores numéricos da intensidade das pertubações
-            #por valores de texto, bem como, adicionar esta composição a variável perturbacao_final
-            for chave, n in selec_final_dicionario.items():
-
-                if n == 1:
-                    texto_add = 'leve'
-                    
-                elif n == 2:
-                    texto_add = 'moderada'
-                    
-                elif n == 3:
-                    texto_add = 'severa'
-               
-                else:
-                    texto_add = 'ausente'
                 
-                perturbacao_final.append(f'{chave} {texto_add}')           
+            ##teste de classificação para as duas perturbações mais influentes, começando pelo caso 0, onde nenhuma perturbação foi identificada
+            if estiagem == 0 and erosao == 0 and fogo == 0 and desmatamento == 0 and pastoreio == 0 and inundacao == 0:
 
-        #etapa 04 composição do nome e aplicação no dataframe em sua respectiva coluna 
-        df_nomear.loc[i,'paisagem'] = str(df_nomear['fisionomia'][i]) + ' ' + str(df_nomear['complementos'][i]) + ' sobre ambiente ' + str(df_nomear['ambiente'][i]) + ', influenciado por ' + str(perturbacao_final[0])+ ' e '+ str(perturbacao_final[1])
-       
+                perturbacao_final = 'sem perturbações visíveis'
+            
+            else:
+                #para facilitar a seleção entre as 6 variáveis foi criado um dicionário que posteriomente será classificado e extraído os dois maiores valores
+                seletor = {'estiagem': estiagem, 'erosão': erosao,'fogo': fogo, 'desmatamento': desmatamento, 'pastoreio/herbivoria': pastoreio, 'inundação': inundacao}
+
+                seletor_ordenado = sorted(seletor.items(), key=lambda x: x[1], reverse=True)
+
+                selec_final = seletor_ordenado[:2]
+                selec_final_dicionario = dict(selec_final)
+            
+                #for para percorrer o dicionário contendo as perturbações de maior influência e substituir os valores numéricos da intensidade das pertubações
+                #por valores de texto, bem como, adicionar esta composição a variável perturbacao_final
+                for chave, n in selec_final_dicionario.items():
+
+                    if n == 1:
+                        texto_add = 'leve'
+                        
+                    elif n == 2:
+                        texto_add = 'moderada'
+                        
+                    elif n == 3:
+                        texto_add = 'severa'
+                
+                    else:
+                        texto_add = 'ausente'
+                    
+                    perturbacao_final.append(f'{chave} {texto_add}')           
+
+            #etapa 04 composição do nome e aplicação no dataframe em sua respectiva coluna 
+            df_nomear.loc[i,'paisagem'] = str(df_nomear['fisionomia'][i]) + ' ' + str(df_nomear['complementos'][i]) + ' sobre ambiente ' + str(df_nomear['ambiente'][i]) + ', influenciado por ' + str(perturbacao_final[0])+ ' e '+ str(perturbacao_final[1])
+        
     return df_nomear
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#função para coletar dados prévios para abertura de csv
+#Entrada da função: nenhuma
+#saída da função: caminho do arquivo e nome
+def leitura_r():
+    
+    #variaveis de universo local
+    caminho = ''
+    nome = ''
+    caminho_final = ''
+    #layout de abertura
+    layout_abertura = [
+           [sg.Text('Digite o caminho onde o arquivo está armazenado(ex: C:\Pastas\pasta):')],
+           [sg.InputText()],
+           [sg.Text('Digite o nome do arquivo e a respectiva extensão:')],
+           [sg.InputText()],
+           [sg.Button('Ok'), sg.Button('Sair')]
+           ]
+    
+    abertura = sg.Window('abertura',layout_abertura)
+
+    while True:
+
+        event, values = abertura.read()
+        if event == sg.WINDOW_CLOSED or event == 'Sair':
+            abertura.close()
+            break
+
+        elif event == 'Ok':
+            caminho = values[0]
+            nome = values[1]
+
+
+        # Executa a função 1 com os valores recebidos
+        # Define o layout da segunda tela
+        confirmacao = [
+                    [sg.Text(f'O caminho do arquivo é: {caminho}')],
+                    [sg.Text(f'O nome arquivo é: {nome}')],
+                    [sg.Button('Voltar'), sg.Button('Ok')]]
+        
+        # Cria a janela da segunda tela
+        confirma = sg.Window('Tela 2', confirmacao)
+
+        # Loop para capturar as interações do usuário com a segunda tela
+        while True:
+            event, values = confirma.read()
+            if event == sg.WINDOW_CLOSED or event == 'Ok':
+                confirma.close()
+                break
+            elif event == 'Voltar':
+                # Fecha a janela da segunda tela e volta para a primeira tela
+                confirma.close()
+                break
+        # Fecha a janela da primeira tela
+        abertura.close()
+
+        caminho_final = r'{}'.format(caminho+'/'+nome)
+    
+        return caminho_final
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#função para coletar dados prévios para salvamento 
+#Entrada da função: nenhuma
+#saída da função: caminho do arquivo e nome para salvamento
+def salvar_w():
+    
+    #variaveis de universo local
+    caminho = ''
+    nome = ''
+
+    #layout de abertura
+    layout_salvamento = [
+           [sg.Text('Digite o caminho onde deseja salvar o arquivo GEOPACKAGE(ex: C:\Pastas\pasta):')],
+           [sg.InputText()],
+           [sg.Text('Digite o nome que deseja colocar no GEOPACKAGE:')],
+           [sg.InputText()],
+           [sg.Button('Ok'), sg.Button('Sair')]
+           ]
+    
+    salvamento = sg.Window('salvamento',layout_salvamento)
+
+    while True:
+
+        event, values = salvamento.read()
+        if event == sg.WINDOW_CLOSED or event == 'Sair':
+            salvamento.close()
+            break
+
+        elif event == 'Ok':
+            caminho = values[0]
+            nome = values[1]
+
+        # Executa a função 1 com os valores recebidos
+        # Define o layout da segunda tela
+        confirmacao = [
+                    [sg.Text(f'O caminho do arquivo será: {caminho}')],
+                    [sg.Text(f'O nome arquivo será: {nome}')],
+                    [sg.Button('Voltar'), sg.Button('Ok')]]
+        
+        # Cria a janela da segunda tela
+        confirma = sg.Window('Tela 2', confirmacao)
+
+        # Loop para capturar as interações do usuário com a segunda tela
+        while True:
+            event, values = confirma.read()
+            if event == sg.WINDOW_CLOSED or event == 'Ok':
+                confirma.close()
+                break
+            elif event == 'Voltar':
+                # Fecha a janela da segunda tela e volta para a primeira tela
+                confirma.close()
+                break
+        # Fecha a janela da primeira tela
+        salvamento.close()
+
+        caminho_final = r'{}'.format(caminho+'/'+nome)
+    
+        return caminho_final
--- code/main.py
+++ code/main.py
@@ -5,23 +5,59 @@ Project: Otimização de etapas em cartografia de paisagens
 
 #imports
 import functions as fun
-import pandas as pd
+import PySimpleGUI as sg
+
+#variáveis do universo
+caminho_abertura = ''
+caminho_salvamento = ''
+df_carga = ''
+df_padronizado = ''
+df_rename = ''
+
+#interfaces
+layout_menu = [
+          [sg.Text('Bem vindo a ferramento de otimização de etapas em cartogafia de paisagens')],
+          [sg.Text('Escolha uma função:')],
+          [sg.Text('Carregar dados do ficha sugerida em CSV e realizar a padronização dos dados:')],
+          [sg.Button('Carregar arquivo')],
+          [sg.Text('Aplicar sugestões de nome a paisagem baseado na proposta de Cavalcanti,L.C.,2018:')],
+          [sg.Button('Sugerir nome da paisagem e salvar')],
+          [sg.Text('Cria arquivo GEOPACKGE sem a aplicação da metodologia:')],
+          [sg.Button('Salvar como GEOPACKGE direto do arquivo carregado')],
+          [sg.Button('Sair')]
+          ]
+
+window = sg.Window('Minha janela', layout_menu)
 
 
-df_abertura = pd.read_csv(r'C:\TCC\TCC_CART_PAISAGENS\CSV_TCC_V0013.csv', sep=',',encoding='utf-16')
 
-#df_abertura = fun.open_csv()
+#menu principal com telas
+while True:
 
-print(df_abertura.head(4))
+    event, values = window.read()
+    #casos de interrução
+    if event == sg.WINDOW_CLOSED or event == 'Sair':
+        break
+    
+    #procedimento de carga do arquivo
+    elif event == 'Carregar arquivo':
+    
+        caminho_abertura = fun.leitura_r()
+        df_carga = fun.open_csv(caminho_abertura)
+        df_padronizado = fun.validar_dados_df(df_carga)
+       
+    #salvamento com sugestão de nome
+    elif event == 'Sugerir nome da paisagem e salvar':
+
+        df_rename = fun.paisagem_nome(df_padronizado)
+        caminho_salvamento = fun.salvar_w()
+        fun.creat_geopackge(df_rename,caminho_salvamento)
+
+    #salvamento sem sugestão de nome
+    elif event == 'Salvar como GEOPACKGE direto do arquivo carregado':
+        
+        caminho_salvamento = fun.salvar_w()
+        fun.creat_geopackge(df_padronizado,caminho_salvamento)
 
 
 
-df_validado = fun.validar_dados_df(df_abertura)
-
-print(df_validado.head(5))
-print(df_validado.dtypes)
-
-df_nomeado = fun.paisagem_nome(df_validado)
-
-print(df_nomeado.head(5))
-df_nomeado.to_csv('C:\TCC\TCC_CART_PAISAGENS\df_nomeado.csv', index=False)
